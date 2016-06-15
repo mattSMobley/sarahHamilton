5ef11984e7f9e5713b93eb516ec4f95854e05292
Matt Mobley <mattsmobley@gmail.com>
2016-06-15 15:56:38 -0600

    changing the color schemes

--- bat/MailHandler-sub.php
+++ bat/MailHandler-sub.php
@@ -5,15 +5,15 @@
 
 	$name=$_POST['name'];
 	$email=$_POST['email'];
-	$owner=$_POST['owner'];
-	$owner_email=$_POST['owner_email'];
-	$sitename=$_POST['sitename'];
+	$owner=$_POST['Sarah Hamilton'];
+	$owner_email=$_POST['mattsmobley@gmail.com'];
+	$sitename=$_POST['Sarah Hamilton Photography'];
 	$sbj_visitor.=$sitename;
 	$sbj_owner.=$sitename;
-	
-	$msg_visitor='<a href="http://'.$sitename.'">'.$sitename.'</a>'."\n".'<br>'.'Hi, '.$name."\n".'<br>'.'Thank you for subscribing to our newsletter!';		
-	$msg_owner='<a href="http://'.$sitename.'">'.$sitename.'</a>'."\n".'<br>'.'This email has been sent via newsletter subscription form on your website. A new visitor would like to be added to your website\'s newsletter:'."\n".'<br>'.'Visitor name: '.$name."\n".'<br>'.'Visitor email: '.$email."\n".'<br>'.'Please add him (her) to your newsletter list.';
-	
+
+	$msg_visitor='<a href="http://'.$sitename.'">'.$sitename.'</a>'."\n".'<br>'.'Hi, '.$name."\n".'<br>'.'Thanks for submitting a query on our website. I will be in contact with you as soon as possible with answers to all your questions!';
+	$msg_owner='<a href="http://'.$sitename.'">'.$sitename.'</a>'."\n".'<br>'.'This email has been sent via the contact form on your website. A new visitor has questions'."\n".'<br>'.'Visitor name: '.$name."\n".'<br>'.'Visitor email: '.$email."\n".'<br>';
+
 	try{
 		if(!mail($email,$sbj_visitor,$msg_visitor,$header.'From: '.$owner_email)){
 			throw new Exception('mail failed');
@@ -23,7 +23,7 @@
 	}catch(Exception $e){
 		echo $e->getMessage() ."\n";
 	}
-	
+
 	try{
 		if(!mail($owner_email,$sbj_owner,$msg_owner,$header.'From: '.$email)){
 			throw new Exception('mail failed');
